db9cc0fd27e9784b9298eab181f1d6e186effdca
carmeldev <carmeldevj@gmail.com>
2025-05-20 18:31:41 +0530

    DB changes

--- db.js
+++ db.js
@@ -8,29 +8,29 @@ const { getEpraSecrets } = require('./services/web/secretManager.service');
 //   logging: false
 // });
 
-// const sequelize = new Sequelize('epra_journal', 'admin', 'Epra&DB^!23', {
-//   dialect: 'mysql',
-//   host: 'epra-journal.cd6sw8wk8fww.ap-south-1.rds.amazonaws.com',
-//   logging: false
-// });
-let sequelize;
+const sequelize = new Sequelize('epra_journal', 'admin', 'Epra&DB^!23', {
+  dialect: 'mysql',
+  host: 'epra-journal.cd6sw8wk8fww.ap-south-1.rds.amazonaws.com',
+  logging: false
+});
+// let sequelize;
 
-(async () => {
-  try {
-    const secret = await getEpraSecrets();
+// (async () => {
+//   try {
+//     const secret = await getEpraSecrets();
 
-    sequelize = new Sequelize(secret.DB_NAME, secret.DB_USER, secret.DB_PASSWORD, {
-      host: secret.DB_HOST,
-      dialect: 'mysql',
-      logging: false,
-    });
+//     sequelize = new Sequelize(secret.DB_NAME, secret.DB_USER, secret.DB_PASSWORD, {
+//       host: secret.DB_HOST,
+//       dialect: 'mysql',
+//       logging: false,
+//     });
 
-    await sequelize.authenticate();
-    console.log('Database connection established using Secrets Manager.');
-  } catch (err) {
-    console.error('Unable to connect to the database:', err);
-    process.exit(1);
-  }
-})();
+//     await sequelize.authenticate();
+//     console.log('Database connection established using Secrets Manager.');
+//   } catch (err) {
+//     console.error('Unable to connect to the database:', err);
+//     process.exit(1);
+//   }
+// })();
 
 module.exports = sequelize;
